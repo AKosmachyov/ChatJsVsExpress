285b9b550b2c8908c131a246b5da2e61ace7a3ae
Alexander Peter <Alexander Peter>
2016-12-12 09:09:18 +0300

    Added new functionality for authorization users

--- model/DB.js
+++ model/DB.js
@@ -1,49 +1,56 @@
-/**
- * Created by Alexander on 09.03.2016.
- */
-var MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/Chat';
+const url = 'mongodb://localhost:27017/Chat';
 var collection;
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected")
     collection = db.collection('Users');
 });
+const UserEntity = function ({userName, email, password}) {
+    return{
+        userName: userName,
+        email: email,
+        password: password,
+        avatarLink: "images/logo.jpg"
+    }
+};
 
 function isValidUserRegister(user) {
-    return (!!user && isValidEmail(user.email) && !!user.name && !!user.password);
+    return (!!user && isValidEmail(user.email) && !!user.userName && !!user.password);
+}
+function isValidUserLogin(user){
+    return (!!user && isValidEmail(user.email) && !!user.password)
 }
 function isValidEmail(email) {
     return (/\S+@\S+\.\S+/).test(email);
-}
-function countUsersByEmail(email) {
-    //return collection.findOne({email: email},{fields:{_id:1}});
-    return collection.find({email: email}).limit(1).count();
 }
 
 var userStorage = {
     addNewUser: function (user) {
         if (!isValidUserRegister(user))
             return Promise.reject("User entity is incorrect");
-        return countUsersByEmail(user.email)
+        return collection.find({$or:[{userName: user.userName}, {email: user.email}]}).count()
             .then(function (count) {
                 if (count > 0) {
                     throw new Error("This email is already registered");
                 }
-                var newUser = {
-                    userName: user.name,
-                    email: user.email,
-                    password: user.password,
-                    avatarLink: "images/logo.jpg"
-                };
-                return collection.insertOne(newUser)
+                return collection.insertOne(new UserEntity(user))
             });
     },
-    checkPassword: function (user) {
-
+    login: function (user) {
+        if(!isValidUserLogin(user))
+            return Promise.reject("User entity is incorrect");
+        return collection.find(user).next()
+            .then(function (val) {
+                if(val === null)
+                    throw new Error("Email or password is incorrect");
+                return {
+                    userName: val.userName,
+                    avatarLink: val.avatarLink
+                }
+            })
     },
     changePassword: function(user){
 
